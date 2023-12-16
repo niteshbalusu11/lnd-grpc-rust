2f3e47fa95432cc13f88edfa9c9bc11ec12990de
Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>
2023-12-16 19:36:21 +0530

    fix: tokio features for examples
    Signed-off-by: Nitesh Balusu <84944042+niteshbalusu11@users.noreply.github.com>

--- examples/addholdinvoice.rs
+++ examples/addholdinvoice.rs
@@ -2,18 +2,17 @@ use std::fs;
 
 #[tokio::main]
 async fn main() {
-        // Read the contents of the file into a vector of bytes
-        let cert_bytes = fs::read("path/to/tlscert").expect("FailedToReadTlsCertFile");
-        let mac_bytes = fs::read("path/to/macaroon").expect("FailedToReadMacaroonFile");
-    
-    
-            // Convert the bytes to a hex string
-        let cert = buffer_as_hex(cert_bytes);
-        let macaroon = buffer_as_hex(mac_bytes);
-    
-        let socket = "localhost:10001".to_string();
-    
-        let mut client = lnd_grpc_rust::connect(cert, macaroon, socket)
+    // Read the contents of the file into a vector of bytes
+    let cert_bytes = fs::read("path/to/tlscert").expect("FailedToReadTlsCertFile");
+    let mac_bytes = fs::read("path/to/macaroon").expect("FailedToReadMacaroonFile");
+
+    // Convert the bytes to a hex string
+    let cert = buffer_as_hex(cert_bytes);
+    let macaroon = buffer_as_hex(mac_bytes);
+
+    let socket = "localhost:10001".to_string();
+
+    let mut client = lnd_grpc_rust::connect(cert, macaroon, socket)
         .await
         .expect("failed to connect");
 
@@ -33,7 +32,10 @@ async fn main() {
 }
 
 fn buffer_as_hex(bytes: Vec<u8>) -> String {
-    let hex_str = bytes.iter().map(|b| format!("{:02x}", b)).collect::<String>();
+    let hex_str = bytes
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>();
 
     return hex_str;
 }
--- examples/subscribe_invoices.rs
+++ examples/subscribe_invoices.rs
@@ -7,14 +7,13 @@ async fn main() {
     // Read the contents of the file into a vector of bytes
     let cert_bytes = fs::read("path/to/tlscert").expect("FailedToReadTlsCertFile");
     let mac_bytes = fs::read("path/to/macaroon").expect("FailedToReadMacaroonFile");
-    
-    
+
     // Convert the bytes to a hex string
     let cert = buffer_as_hex(cert_bytes);
     let macaroon = buffer_as_hex(mac_bytes);
-    
+
     let socket = "localhost:10001".to_string();
-    
+
     let mut client = lnd_grpc_rust::connect(cert, macaroon, socket)
         .await
         .expect("failed to connect");
@@ -34,9 +33,7 @@ async fn main() {
         .await
         .expect("Failed to receive invoices")
     {
-        if let Some(state) =
-            lnd_grpc_rust::lnrpc::invoice::InvoiceState::from_i32(invoice.state)
-        {
+        if let Some(state) = lnd_grpc_rust::lnrpc::invoice::InvoiceState::from_i32(invoice.state) {
             // If this invoice was Settled we can do something with it
             if state == lnd_grpc_rust::lnrpc::invoice::InvoiceState::Settled {
                 println!("{:?}", invoice);
@@ -46,7 +43,10 @@ async fn main() {
 }
 
 fn buffer_as_hex(bytes: Vec<u8>) -> String {
-    let hex_str = bytes.iter().map(|b| format!("{:02x}", b)).collect::<String>();
+    let hex_str = bytes
+        .iter()
+        .map(|b| format!("{:02x}", b))
+        .collect::<String>();
 
     return hex_str;
 }
